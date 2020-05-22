8a88bafdc4a20fc1dd8a1226201b885bead76b64
Luiz Guilherme D'Abruzzo Pereira <luiz290788@gmail.com>
2020-05-22 07:27:00 -0300

    remove excelize and use zip to read xlsx to save memory

--- data-xlsx.go
+++ data-xlsx.go
@@ -1,12 +1,14 @@
 package covid19brazilimporter
 
 import (
+	"archive/zip"
+	"bytes"
+	"encoding/xml"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
 	"strings"
-
-	excelize "github.com/360EntSecGroup-Skylar/excelize/v2"
 )
 
 const stateCol = 1
@@ -19,61 +21,62 @@ const deathsCol = 11
 // XLSXReader is a data reader for xlsx files
 type XLSXReader struct{}
 
+type xlsxC struct {
+	R string `xml:"r,attr,omitempty"`
+	V string `xml:"v,omitempty"`
+}
+
+type xlsxSi struct {
+	T string `xml:"t"`
+}
+
+type xslxSst struct {
+	Count int `xml:"count,attr,omitempty"`
+}
+
 func (XLSXReader) read(fileURL string) (chan *Entry, error) {
 	dataChannel := make(chan *Entry)
 
 	go func() {
-		resp, _ := http.Get(fileURL)
-		file, fileError := excelize.OpenReader(resp.Body)
 		defer close(dataChannel)
-		if fileError != nil {
-			log.Panicln(fileError.Error())
-			return
-		}
+		resp, _ := http.Get(fileURL)
+		body, _ := ioutil.ReadAll(resp.Body)
+		reader, _ := zip.NewReader(bytes.NewReader(body), int64(len(body)))
+		var stringTable []*string = nil
+		for _, file := range reader.File {
+			if file.Name == "xl/sharedStrings.xml" {
+				stringTable, _ = readSharedStrings(file)
+			} else if file.Name == "xl/worksheets/sheet1.xml" {
+				rows, _ := readRows(file, stringTable)
+				for _, row := range rows {
+					state := row[stateCol]
+					if state == nil {
+						// ignore country entries
+						continue
+					}
 
-		rows, rowsError := file.Rows("Sheet 1")
-		if rowsError != nil {
-			log.Panicln(rowsError.Error())
-			return
-		}
-		// ignore header
-		rows.Next()
-		// we need to call columns to thrash the header columns
-		rows.Columns()
+					city := row[cityCol]
+					if city != nil {
+						// ignore city entries for now
+						continue
+					}
 
-		for rows.Next() {
-			row, columnsErr := rows.Columns()
-			if columnsErr != nil {
-				log.Printf("WARN: error reading xlsx file %v", columnsErr.Error())
-				continue
-			}
+					codMun := row[codMunCol]
+					if codMun != nil {
+						// ignore numbers of the state without city
+						continue
+					}
 
-			state := row[stateCol]
-			if len(strings.TrimSpace(state)) == 0 {
-				// ignore country entries
-				continue
-			}
-
-			city := row[cityCol]
-			if len(strings.TrimSpace(city)) > 0 {
-				// ignore city entries for now
-				continue
-			}
-
-			codMun := row[codMunCol]
-			if len(strings.TrimSpace(codMun)) > 0 {
-				// ignore numbers of the state without city
-				continue
-			}
-
-			date, _ := parseDate(row[dateCol])
-			cases, _ := strconv.Atoi(row[casesCol])
-			deaths, _ := strconv.Atoi(row[deathsCol])
-			dataChannel <- &Entry{
-				Region: row[stateCol],
-				Date:   date,
-				Cases:  cases,
-				Deaths: deaths,
+					date, _ := parseDate(*row[dateCol])
+					cases, _ := strconv.Atoi(*row[casesCol])
+					deaths, _ := strconv.Atoi(*row[deathsCol])
+					dataChannel <- &Entry{
+						Region: *row[stateCol],
+						Date:   date,
+						Cases:  cases,
+						Deaths: deaths,
+					}
+				}
 			}
 		}
 	}()
@@ -83,4 +86,95 @@ func (XLSXReader) read(fileURL string) (chan *Entry, error) {
 
 func (XLSXReader) supports(fileURL string) bool {
 	return strings.HasSuffix(fileURL, ".xlsx")
+}
+
+func readSharedStrings(file *zip.File) (strs []*string, err error) {
+	reader, _ := file.Open()
+	decoder := xml.NewDecoder(reader)
+
+	index := 0
+	for {
+		token, _ := decoder.Token()
+		if token == nil {
+			return
+		}
+		switch actualToken := token.(type) {
+		case xml.StartElement:
+			if actualToken.Name.Local == "si" {
+				sharedString := &xlsxSi{}
+				decoder.DecodeElement(sharedString, &actualToken)
+				strs[index] = &sharedString.T
+				index++
+			} else if actualToken.Name.Local == "sst" {
+				for _, attr := range actualToken.Attr {
+					if attr.Name.Local == "count" {
+						count, _ := strconv.Atoi(attr.Value)
+						strs = make([]*string, count)
+					}
+				}
+			}
+			break
+		}
+	}
+}
+
+func readRows(file *zip.File, stringTable []*string) ([][]*string, error) {
+	reader, sheetErr := file.Open()
+	if sheetErr != nil {
+		log.Fatalf("error opening file: %v", sheetErr.Error())
+	}
+
+	decoder := xml.NewDecoder(reader)
+
+	var rows [][]*string
+
+	for {
+		token, _ := decoder.Token()
+
+		if token == nil {
+			break
+		}
+		switch actualToken := token.(type) {
+		case xml.StartElement:
+			if actualToken.Name.Local == "row" {
+				row, _ := readRow(decoder, actualToken, stringTable)
+				rows = append(rows, row)
+			}
+			break
+		}
+	}
+	return rows, nil
+}
+
+func readRow(decoder *xml.Decoder, start xml.StartElement, stringTable []*string) (row []*string, err error) {
+	for {
+		token, tokenErr := decoder.Token()
+		if tokenErr != nil {
+			return
+		}
+
+		if token == nil {
+			break
+		}
+
+		switch actualToken := token.(type) {
+		case xml.StartElement:
+			if actualToken.Name.Local == "c" {
+				value := &xlsxC{}
+				decoder.DecodeElement(value, &actualToken)
+				index, indexErr := strconv.Atoi(value.V)
+				if indexErr != nil {
+					row = append(row, nil)
+				} else {
+					row = append(row, stringTable[index])
+				}
+			}
+		case xml.EndElement:
+			if actualToken.Name.Local == "row" {
+				return
+			}
+			break
+		}
+	}
+	return
 }
--- go.mod
+++ go.mod
@@ -4,9 +4,14 @@ go 1.13
 
 require (
 	cloud.google.com/go/firestore v1.2.0
-	github.com/360EntSecGroup-Skylar/excelize/v2 v2.2.0
 	github.com/GoogleCloudPlatform/functions-framework-go v1.0.1
 	github.com/google/go-github v17.0.0+incompatible
 	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/stretchr/testify v1.5.1 // indirect
+	golang.org/x/net v0.0.0-20200506145744-7e3656a0809f // indirect
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
